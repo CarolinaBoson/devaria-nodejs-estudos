850c20750ffb8b46ff71bcdd615cc79453e24595
carolinaboson@gmail.com <carolinaboson@gmail.com>
2023-03-29 02:13:23 -0300

    estruturas de repetição

---
+++ hello-world-nextjs/pages/api/data.ts
@@ -0,0 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from 'next'; 
+import moment from 'moment';
+export default (req : NextApiRequest, res : NextApiResponse) => {
+	res.status(200).json({ data : moment().format('DD/MM/yyyy HH:mm:ss')});
+};
